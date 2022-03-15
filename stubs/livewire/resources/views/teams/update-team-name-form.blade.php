914cca9950901ea8d004a8cbaa0f558b7660113a
monab <monajmakwana@gmail.com>
2022-03-15 13:44:01 +0530

    fix(ui): spacing between user image and text

--- stubs/livewire/resources/views/teams/update-team-name-form.blade.php
+++ stubs/livewire/resources/views/teams/update-team-name-form.blade.php
@@ -17,7 +17,7 @@
       <x-jet-label class="form-label" value="{{ __('Team Owner') }}" />
 
       <div class="d-flex mt-1">
-        <img class="rounded-circle me-1" width="48" src="{{ $team->owner->profile_photo_url }}">
+        <img class="rounded-circle me-2" width="48" src="{{ $team->owner->profile_photo_url }}">
         <div>
           <div>{{ $team->owner->name }}</div>
           <div class="text-muted">{{ $team->owner->email }}</div>
@@ -29,20 +29,19 @@
     <div class="mb-3">
       <x-jet-label class="form-label" for="name" value="{{ __('Team Name') }}" />
 
-      <x-jet-input id="name" type="text" class="{{ $errors->has('name') ? 'is-invalid' : '' }}"
-        wire:model.defer="state.name" :disabled="! Gate::check('update', $team)" />
+      <x-jet-input id="name" type="text" class="{{ $errors->has('name') ? 'is-invalid' : '' }}" wire:model.defer="state.name" :disabled="! Gate::check('update', $team)" />
 
       <x-jet-input-error for="name" />
     </div>
   </x-slot>
 
   @if (Gate::check('update', $team))
-    <x-slot name="actions">
-      <div class="d-flex align-items-baseline">
-        <x-jet-button>
-          {{ __('Save') }}
-        </x-jet-button>
-      </div>
-    </x-slot>
+  <x-slot name="actions">
+    <div class="d-flex align-items-baseline">
+      <x-jet-button>
+        {{ __('Save') }}
+      </x-jet-button>
+    </div>
+  </x-slot>
   @endif
 </x-jet-form-section>
