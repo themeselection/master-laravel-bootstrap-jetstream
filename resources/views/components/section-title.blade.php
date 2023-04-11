5b8fa434ce689c897369bf23253b1f2208f5524d
monab <monajmakwana@gmail.com>
2023-04-11 11:17:18 +0530

    refactor(components): added new components files - updated content as per bootstrap - updated installCommand file to replace layout pages

---
+++ resources/views/components/section-title.blade.php
@@ -0,0 +1,9 @@
+<div class="col-md-6 d-flex justify-content-between">
+    <div class="px-4">
+        <h3 class="text-lg font-medium text-gray-900">{{ $title }}</h3>
+
+        <p class="mt-1 text-sm text-gray-600">{{ $description }}</p>
+    </div>
+
+    <div class="px-4">{{ $aside ?? '' }}</div>
+</div>
--- src/Themeselection/Jetstrap/Console/InstallCommand.php
+++ src/Themeselection/Jetstrap/Console/InstallCommand.php
@@ -77,6 +77,9 @@ class InstallCommand extends Command
 
     // add components in navbar
     $this->replaceInFile('{{-- <x-switchable-team :team="$team" /> --}}', '<x-switchable-team :team="$team" />', resource_path('views/layouts/sections/navbar/navbar.blade.php'));
+    $this->replaceInFile('{{-- <x-banner /> --}}', '<x-banner />', resource_path('views/layouts/contentNavbarLayout.blade.php'));
+    $this->replaceInFile('{{-- <x-banner /> --}}', '<x-banner />', resource_path('views/layouts/horizontalLayout.blade.php'));
+    $this->replaceInFile('{{-- <x-banner /> --}}', '<x-banner />', resource_path('views/layouts/navbarFullLayout.blade.php'));
 
 
     // Bootstrap Configuration...
