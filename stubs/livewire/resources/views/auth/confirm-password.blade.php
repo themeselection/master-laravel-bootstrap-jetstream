cc1e3e88cfe4c35c7bf767c0077282254f9c86dd
smit patel <bhalodiyasmit2409@gmail.com>
2023-08-18 13:20:25 +0530

    refactor(auth): change confirm-password.blade - remove forgot password

--- stubs/livewire/resources/views/auth/confirm-password.blade.php
+++ stubs/livewire/resources/views/auth/confirm-password.blade.php
@@ -57,19 +57,8 @@ $customizerHidden = 'customizer-hide';
             </span>
             @enderror
           </div>
-
-          <button type="submit" class="btn btn-primary d-grid w-100 mb-3">
-            Confirm Password
-          </button>
+          <button type="submit" class="btn btn-primary d-grid w-100 mb-3">Confirm Password</button>
         </form>
-
-        <p class="text-center mt-2">
-          @if (Route::has('password.request'))
-          <a class="btn btn-link" href="{{ route('password.request') }}">
-            {{ __('Forgot Your Password?') }}
-          </a>
-          @endif
-        </p>
       </div>
     </div>
     <!-- / password confirm -->
